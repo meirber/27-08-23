4d64c5c60d7bdc624efa9400567c44238589c62f
meirber <marb051297@gmail.com>
2023-08-28 15:27:34 +0300

    28-08-23

--- oop2.js
+++ oop2.js
@@ -14,64 +14,104 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Person = /** @class */ (function () {
-    function Person(firstName, lastName) {
+    function Person(firstName, lastName, age, address) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
+        this.address = address;
     }
+    Person.prototype.getInfo = function () {
+        return "full name: ".concat(this.firstName, " ").concat(this.lastName);
+    };
     return Person;
 }());
 var Patient = /** @class */ (function (_super) {
     __extends(Patient, _super);
-    function Patient(patientID, firstName, lastName) {
-        var _this = _super.call(this, firstName, lastName) || this;
+    function Patient(patientID, firstName, lastName, age, address, phoneNumbe, emergencyContact, medicalHistory) {
+        var _this = _super.call(this, firstName, lastName, age, address) || this;
         _this.patientID = patientID;
+        _this.phoneNumbe = phoneNumbe;
+        _this.emergencyContact = emergencyContact;
+        _this.medicalHistory = medicalHistory;
         return _this;
     }
     ;
-    Patient.prototype.getPatientInfo = function () {
+    Patient.prototype.getInfo = function () {
         return "patientID: ".concat(this.patientID, ", name: ").concat(this.firstName, " ").concat(this.lastName);
+    };
+    Patient.prototype.addMedicalHistory = function (appointment) {
+        this.medicalHistory.push(appointment);
     };
     return Patient;
 }(Person));
 // const newPatuent = new Patient(10, "meir", "ber");
 // console.log(newPatuent);
-// newPatuent.getPatientInfo();
-var Doctor = /** @class */ (function (_super) {
-    __extends(Doctor, _super);
-    function Doctor(doctorID, specialization, firstName, lastName) {
-        var _this = _super.call(this, firstName, lastName) || this;
-        _this.doctorID = doctorID;
-        _this.specialization = specialization;
+// newPatuent.getInfo();
+var MedicalStaff = /** @class */ (function (_super) {
+    __extends(MedicalStaff, _super);
+    function MedicalStaff(firstName, lastName, age, address, staffID, position, department) {
+        var _this = _super.call(this, firstName, lastName, age, address) || this;
+        _this.staffID = staffID;
+        _this.position = position;
+        _this.department = department;
         return _this;
     }
-    Doctor.prototype.getDoctorInfo = function () {
+    return MedicalStaff;
+}(Person));
+var Doctor = /** @class */ (function (_super) {
+    __extends(Doctor, _super);
+    function Doctor(doctorID, specialization, firstName, lastName, age, address, staffID, position, department, availability) {
+        var _this = _super.call(this, firstName, lastName, age, address, staffID, position, department) || this;
+        _this.doctorID = doctorID;
+        _this.specialization = specialization;
+        _this.availability = availability;
+        return _this;
+    }
+    Doctor.prototype.getInfo = function () {
         return "doctor id: ".concat(this.doctorID, ", name: ").concat(this.firstName, " ").concat(this.lastName, ", specialization: ").concat(this.specialization);
     };
+    Doctor.prototype.isAvailable = function (date) {
+        for (var _i = 0, _a = this.availability; _i < _a.length; _i++) {
+            var slot = _a[_i];
+            if (date.toLocaleDateString === slot.toLocaleDateString) {
+                return false;
+            }
+        }
+        return true;
+    };
     return Doctor;
-}(Person));
+}(MedicalStaff));
 // const newDoctor = new Doctor(10, "tooth", "meir", "ber");
 // console.log(newDoctor);
-// newDoctor.getDoctorInfo();
+// newDoctor.getInfo();
 var Appointment = /** @class */ (function () {
-    function Appointment(patient, doctor, date, time) {
+    function Appointment(patient, doctor, date, time, status) {
         this.patient = patient;
         this.doctor = doctor;
         this.date = new Date(date);
         this.time = time;
+        this.status = "Planned";
     }
-    Appointment.prototype.getAppointmentInfo = function () {
-        console.log("".concat(this.patient.getPatientInfo(), ", ").concat(this.doctor.getDoctorInfo(), ", date: ").concat(this.date.toLocaleDateString(), ", time: ").concat(this.time));
+    Appointment.prototype.markAsCompleted = function () {
+        this.status = "Completed";
+    };
+    Appointment.prototype.markAsCancelled = function () {
+        this.status = "Cancelled";
+    };
+    Appointment.prototype.getInfo = function () {
+        console.log("".concat(this.patient.getInfo(), ", ").concat(this.doctor.getInfo(), ", date: ").concat(this.date.toLocaleDateString(), ", time: ").concat(this.time));
     };
     return Appointment;
 }());
-var newAppointment = new Appointment(new Patient(10, "meir", "ber"), new Doctor(1, "family medicine", "moris", "morst"), new Date("2023/08/27"), "14:22");
-newAppointment.getAppointmentInfo();
+// const newAppointment = new Appointment(new Patient(10, "meir", "ber"), new Doctor(1, "family medicine", "moris", "morst"), new Date("2023/08/27"), "14:22")
+// newAppointment.getInfo()
 var Hospital = /** @class */ (function () {
-    function Hospital(patients, doctors, appointments, hospital) {
+    function Hospital(patients, doctors, appointments, hospital, medicalRecord) {
         this.patients = patients;
         this.doctors = doctors;
         this.appointments = appointments;
         this.hospital = hospital;
+        this.medicalRecord = medicalRecord;
     }
     Hospital.prototype.addPatient = function (patient) {
         this.patients.push(patient);
@@ -83,7 +123,7 @@ var Hospital = /** @class */ (function () {
         this.appointments.push(appointment);
     };
     Hospital.prototype.ShowingAllQueues = function () {
-        this.appointments.forEach(function (appointment) { return appointment.getAppointmentInfo(); });
+        this.appointments.forEach(function (appointment) { return appointment.getInfo(); });
     };
     Hospital.prototype.DisplayingDoctorsAppointmentsByID = function (id) {
         var filterAppointments = this.appointments.filter(function (appointment) { return appointment.doctor.doctorID === id; });
@@ -98,5 +138,39 @@ var Hospital = /** @class */ (function () {
         var filterAppointments = this.appointments.filter(function (appointment) { return appointment.date.toLocaleDateString() === today; });
         console.log(filterAppointments);
     };
+    Hospital.prototype.doctorBySpecialty = function (specialization) {
+        var doctorBySpecialty = this.doctors.forEach(function (doctor) { return doctor.specialization === specialization; });
+        console.log(doctorBySpecialty);
+    };
+    Hospital.prototype.createMedicalRecord = function () {
+    };
     return Hospital;
+}());
+var hospital1 = new Hospital([], [], [], "Medical Center", []);
+var patient1 = new Patient(1, "meir", "levi", 24, "dover 6", "0533123243", "0505050555", []);
+hospital1.addPatient(patient1);
+var patient2 = new Patient(2, "shalom", "mor", 27, "dover 9", "0556747657", "0505000000", []);
+hospital1.addPatient(patient2);
+var doctor1 = new Doctor(1, "Cardiology", "noam", "gavish", 50, "molo 56", 8789, 3, 12, [new Date("2023/08/27"), new Date("2023/12/30")]);
+hospital1.addDoctor(doctor1);
+var doctor2 = new Doctor(2, "Pediatrician", "ami", "chohen", 65, "fgh 45", 2324, 1, 10, [new Date("2023/09/27"), new Date("2023/11/20")]);
+hospital1.addDoctor(doctor2);
+var appointment1 = new Appointment(patient1, doctor1, new Date("2023/11/04"), "10:30", "Planned");
+hospital1.addAppointment(appointment1);
+var appointment2 = new Appointment(patient1, doctor2, new Date("2023/11/05"), "13:30", "Planned");
+hospital1.addAppointment(appointment2);
+var appointment3 = new Appointment(patient2, doctor2, new Date("2023/08/28"), "12:00", "Planned");
+hospital1.addAppointment(appointment3);
+hospital1.ShowingAllQueues();
+hospital1.DisplayingDoctorsAppointmentsByID(2);
+hospital1.DisplayingpatientAppointmentsByID(1);
+hospital1.ShowingTodaysQueues();
+var MedicalRecord = /** @class */ (function () {
+    function MedicalRecord(patient, doctor, diagnosis, prescription) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.diagnosis = diagnosis;
+        this.prescription = prescription;
+    }
+    return MedicalRecord;
 }());
--- oop2.ts
+++ oop2.ts
@@ -1,81 +1,137 @@
-class Person {
+abstract class Person {
     firstName: string;
     lastName: string;
-    constructor(firstName: string, lastName: string) {
+    age: number;
+    address: string;
+    constructor(firstName: string, lastName: string, age: number, address: string) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
+
+    getInfo(): string {
+        return `full name: ${this.firstName} ${this.lastName}`
     }
 }
 
 class Patient extends Person {
     patientID: number;
-    constructor(patientID: number, firstName: string, lastName: string) {
-        super(firstName, lastName);
+    phoneNumbe: string;
+    emergencyContact: string;
+    medicalHistory: Appointment[];
+    constructor(patientID: number, firstName: string, lastName: string, age: number, address: string, phoneNumbe: string, emergencyContact: string, medicalHistory: Appointment[]) {
+        super(firstName, lastName, age, address);
         this.patientID = patientID;
+        this.phoneNumbe = phoneNumbe;
+        this.emergencyContact = emergencyContact;
+        this.medicalHistory = medicalHistory;
     };
 
-    getPatientInfo(): string {
+    getInfo(): string {
         return `patientID: ${this.patientID}, name: ${this.firstName} ${this.lastName}`;
+    }
+
+    addMedicalHistory(appointment: Appointment): void {
+        this.medicalHistory.push(appointment)
     }
 }
 // const newPatuent = new Patient(10, "meir", "ber");
 // console.log(newPatuent);
-// newPatuent.getPatientInfo();
+// newPatuent.getInfo();
 
-class Doctor extends Person {
+class MedicalStaff extends Person {
+    staffID: number;
+    position: number;
+    department: number;
+
+    constructor(firstName: string, lastName: string, age: number, address: string, staffID: number, position: number, department: number) {
+        super(firstName, lastName, age, address)
+        this.staffID = staffID;
+        this.position = position;
+        this.department = department;
+    }
+}
+
+class Doctor extends MedicalStaff {
     doctorID: number;
     specialization: string;
-    constructor(doctorID: number, specialization: string, firstName: string, lastName: string) {
-        super(firstName, lastName);
+    availability: Date[];
+    constructor(doctorID: number, specialization: string, firstName: string, lastName: string, age: number, address: string, staffID: number, position: number, department: number, availability: Date[]) {
+        super(firstName, lastName, age, address, staffID, position, department)
         this.doctorID = doctorID;
-        this.specialization = specialization
+        this.specialization = specialization;
+        this.availability = availability;
     }
-    getDoctorInfo(): string {
+    getInfo(): string {
         return `doctor id: ${this.doctorID}, name: ${this.firstName} ${this.lastName}, specialization: ${this.specialization}`;
 
     }
+
+    isAvailable(date: Date): boolean {
+        for (const slot of this.availability) {
+            if (date.toLocaleDateString === slot.toLocaleDateString) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
 // const newDoctor = new Doctor(10, "tooth", "meir", "ber");
 // console.log(newDoctor);
-// newDoctor.getDoctorInfo();
+// newDoctor.getInfo();
 
 class Appointment {
     patient: Patient;
     doctor: Doctor;
     date: Date;
     time: string;
+    status: "Planned" | "Completed" | "Cancelled";
 
-    constructor(patient: Patient, doctor: Doctor, date: Date, time: string) {
+    constructor(patient: Patient, doctor: Doctor, date: Date, time: string, status: "Planned" | "Completed" | "Cancelled") {
         this.patient = patient;
         this.doctor = doctor;
         this.date = new Date(date);
         this.time = time;
+        this.status = "Planned";
+    }
+    markAsCompleted(): void {
+        this.status = "Completed";
     }
 
-    getAppointmentInfo() {
-        console.log(`${this.patient.getPatientInfo()}, ${this.doctor.getDoctorInfo()}, date: ${this.date.toLocaleDateString()}, time: ${this.time}`);
+    markAsCancelled(): void {
+        this.status = "Cancelled";
     }
+    getInfo() {
+        console.log(`${this.patient.getInfo()}, ${this.doctor.getInfo()}, date: ${this.date.toLocaleDateString()}, time: ${this.time}`);
+    }
+
+
 }
 
-const newAppointment = new Appointment(new Patient(10, "meir", "ber"), new Doctor(1, "family medicine", "moris", "morst"), new Date("2023/08/27"), "14:22")
-newAppointment.getAppointmentInfo()
+// const newAppointment = new Appointment(new Patient(10, "meir", "ber"), new Doctor(1, "family medicine", "moris", "morst"), new Date("2023/08/27"), "14:22")
+// newAppointment.getInfo()
 
 class Hospital {
     patients: Patient[];
     doctors: Doctor[];
     appointments: Appointment[];
     hospital: string;
+    medicalRecord: MedicalRecord[];
 
-    constructor(patients: Patient[], doctors: Doctor[], appointments: Appointment[], hospital: string) {
+    constructor(patients: Patient[], doctors: Doctor[], appointments: Appointment[], hospital: string, medicalRecord: MedicalRecord[]) {
         this.patients = patients;
         this.doctors = doctors;
         this.appointments = appointments;
         this.hospital = hospital;
+        this.medicalRecord = medicalRecord;
     }
 
     addPatient(patient: Patient): void {
         this.patients.push(patient)
     }
+
 
     addDoctor(doctor: Doctor): void {
         this.doctors.push(doctor)
@@ -86,22 +142,73 @@ class Hospital {
     }
 
     ShowingAllQueues() {
-        this.appointments.forEach(appointment => appointment.getAppointmentInfo())
+        this.appointments.forEach(appointment => appointment.getInfo())
     }
 
     DisplayingDoctorsAppointmentsByID(id: number): void {
-        let filterAppointments: Appointment[] = this.appointments.filter((appointment: Appointment):boolean => appointment.doctor.doctorID === id) 
+        let filterAppointments: Appointment[] = this.appointments.filter((appointment: Appointment): boolean => appointment.doctor.doctorID === id)
         console.log(filterAppointments);
     }
 
     DisplayingpatientAppointmentsByID(id: number): void {
-        let filterAppointments: Appointment[] = this.appointments.filter((appointment: Appointment):boolean => appointment.patient.patientID === id) 
+        let filterAppointments: Appointment[] = this.appointments.filter((appointment: Appointment): boolean => appointment.patient.patientID === id)
         console.log(filterAppointments);
     }
 
     ShowingTodaysQueues(): void {
         let today = new Date().toLocaleDateString()
-        let filterAppointments: Appointment[] = this.appointments.filter((appointment: Appointment):boolean => appointment.date.toLocaleDateString() === today) 
+        let filterAppointments: Appointment[] = this.appointments.filter((appointment: Appointment): boolean => appointment.date.toLocaleDateString() === today)
         console.log(filterAppointments);
+    }
+
+    doctorBySpecialty(specialization: string): void {
+        const doctorBySpecialty = this.doctors.forEach(doctor => doctor.specialization === specialization);
+        console.log(doctorBySpecialty);
+    }
+    createMedicalRecord() {
+
+    }
+}
+
+const hospital1 = new Hospital([], [], [], "Medical Center", []);
+
+const patient1 = new Patient(1, "meir", "levi", 24, "dover 6", "0533123243", "0505050555", [] );
+hospital1.addPatient(patient1);
+const patient2 = new Patient(2, "shalom", "mor",  27, "dover 9", "0556747657", "0505000000", [] );
+hospital1.addPatient(patient2);
+const doctor1 = new Doctor(1, "Cardiology", "noam", "gavish", 50, "molo 56", 8789, 3, 12, [new Date ("2023/08/27"), new Date ("2023/12/30")]);
+hospital1.addDoctor(doctor1);
+const doctor2 = new Doctor(2, "Pediatrician", "ami", "chohen", 65, "fgh 45", 2324, 1, 10, [new Date ("2023/09/27"), new Date ("2023/11/20")]);
+hospital1.addDoctor(doctor2);
+
+const appointment1 = new Appointment(patient1, doctor1, new Date("2023/11/04"), "10:30", "Planned");
+hospital1.addAppointment(appointment1);
+
+const appointment2 = new Appointment(patient1, doctor2, new Date("2023/11/05"), "13:30","Planned");
+hospital1.addAppointment(appointment2);
+
+const appointment3 = new Appointment(patient2, doctor2, new Date("2023/08/28"), "12:00", "Planned");
+hospital1.addAppointment(appointment3);
+
+hospital1.ShowingAllQueues();
+
+hospital1.DisplayingDoctorsAppointmentsByID(2);
+
+hospital1.DisplayingpatientAppointmentsByID(1);
+
+hospital1.ShowingTodaysQueues();
+
+
+class MedicalRecord {
+    patient: Patient;
+    doctor: Doctor;
+    diagnosis: string;
+    prescription: string;
+
+    constructor(patient: Patient, doctor: Doctor, diagnosis: string, prescription: string) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.diagnosis = diagnosis;
+        this.prescription = prescription;
     }
 }
